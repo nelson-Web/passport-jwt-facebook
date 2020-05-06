0326d972f37b4f27f5d56aa543fadad9641902f7
nelson-Web <62528373+nelson-Web@users.noreply.github.com>
2020-05-05 22:24:41 -0600

    Update database.js

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const {mongodb} = require('./keys')
 
-mongoose.connect(mongodb.URI,{
+mongoose.connect("mongodb://localhost:27017/covid-19",{
 useNewUrlParser:true,
 useUnifiedTopology:true
 }).then(db => console.log('database is connected'))
